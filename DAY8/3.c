84ea4f054063e623b632ff407651e6fe032e080f
ansh1baliyan <ansh.baliyan2025@glbajajgroup.org>
2026-06-13 13:06:31 +0530

    Day8 Uploaded

---
+++ DAY8/3.c
@@ -0,0 +1,17 @@
+//WAP to print character triangle
+#include<stdio.h>
+int main()
+{
+    int rows=5;
+    for(int i=1;i<=rows;i++)
+    {
+        char ch='A';
+        for(int j=1;j<=i;j++)
+        {
+            printf("%c",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
